27d1df0b4eafc0d7a69710a2241cadbb4ecc4af3
mahdihrs <mahdi.haris@myboost.id>
2019-08-01 09:48:45 +0700

    add Constants and FAQ page

---
+++ Constants/Constants.js
@@ -0,0 +1,38 @@
+const Constants = {
+  NAVBAR: {
+    BRAND_LOGO: 'Boost',
+    MENU_1: 'About Us',
+    MENU_2: 'Services',
+    MENU_3: 'Blog',
+    MENU_4: 'FAQ',
+    BUTTON_LEFT: 'Join As Merchant',
+  },
+  HOME_BANNER: {
+    HEADER: "Everyone Deserves Growth",
+    DESCRIPTION: "The fundamental challenge of Indonesia's Financial Inclusion is literacy of the underserved segments.",
+    LEFT_BUTTON: "View More",
+    RIGHT_BUTTON: "See Video"
+  },
+  LANGUAGE: {
+    LANG_1: 'English',
+    LANG_2: 'Indonesian',
+  },
+  LANG_CODES: {
+    LANG_1: 'ENG',
+    LANG_2: 'BAHASA'
+  },
+  HOME_CONTENT: {
+    LEFT_CONTENT: '',
+    HEADER: "Meet Boost, the Digital Economy Platform",
+    DESCRIPTION: "The fundamental challenge of Indonesia's Financial Inclusion is literacy of the underserved segments. To address literacy, users must be educated. Boost disrupt financial literacy education through gamification to make the process more fun.",
+    MORE_BUTTON: "Know More Here ->"
+  },
+  HOME_ABOUT_US: {
+    RIGHT_CONTENT: '',
+    HEADER: "About Us",
+    DESCRIPTION: "Boost is a digital economy platform which provides digital interaction between merchant and consumers (B2C) over a QR Code as a medium to initiate - process - record are interaction through a mobile application at both merchant and consumer end.",
+    MORE_BUTTON: "See Detail Here ->"
+  }
+};
+
+export { Constants };
--- pages/FAQ/index.js
+++ pages/FAQ/index.js
@@ -4,12 +4,15 @@ import SearchBar from '../../components/SearchBarFAQ/SearchBarFAQ.component';
 import FaqContent from '../../components/FaqContent/FaqContent.component';
 import ContactUs from '../../components/ContactUsFaq/ContactsUsFaq.component';
 
+import BeOurPartner from '../../components/BeOurPartner/BeOurPartner.component';
+
 export default function Index() {
   return (
     <Layout>
       <SearchBar />
       <FaqContent />
       <ContactUs />
+      <BeOurPartner />
     </Layout>
   );
 }
