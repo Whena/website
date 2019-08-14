18230104a418125aa4ab6261f4d9a468bc83b598
Muhammad Rizal <muhammad.rizal@myboost.id>
2019-08-14 11:14:24 +0700

    Upgrade package react, react-dom, material-ui/core, material-ui/styles and remove unnecessary package

--- components/Reusable/WhatTheySay/WhatTheySay.component.js
+++ components/Reusable/WhatTheySay/WhatTheySay.component.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import Head from 'next/head';
 import Container from '@material-ui/core/Container';
-import Slider from "react-slick";
+import Slider from 'react-slick';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -9,9 +10,7 @@ import styles from './WhatTheySay.styles';
 import { Constants } from '../../../constants';
 import BottomLiner from '../../BottomLiner/BottomLiner.component';
 
-const {
-  WHATTHEYSAY
-} = Constants;
+const { WHATTHEYSAY } = Constants;
 
 function WhatTheySay(props) {
   const classes = styles();
@@ -24,6 +23,19 @@ function WhatTheySay(props) {
 
   return (
     <Container maxWidth="lg" className={classes.container}>
+      <Head>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charset="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
+      </Head>
       <Typography variant="h4" className={classes.title}>
         What they say?
       </Typography>
@@ -31,13 +43,21 @@ function WhatTheySay(props) {
       <Slider {...settings}>
         {WHATTHEYSAY.map((comment, index) => (
           <Card key={index} className={classes.cards}>
-            <Grid container justify="center" alignItems="center" className={classes.contentContainer}>
+            <Grid
+              container
+              justify="center"
+              alignItems="center"
+              className={classes.contentContainer}
+            >
               <Grid item xs={10}>
                 <CardContent>
                   <Typography variant="h6" className={classes.commentContainer}>
                     {comment.TESTIMONIAL}
                   </Typography>
-                  <Typography variant="h5" className={classes.usernameContainer}>
+                  <Typography
+                    variant="h5"
+                    className={classes.usernameContainer}
+                  >
                     - {comment.USER}
                   </Typography>
                 </CardContent>
